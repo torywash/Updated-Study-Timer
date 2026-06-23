30abcf3bfda811e5a4eb1f7d23361f6233546d65
Tory Wash <washt@uni.edu>
2026-06-23 12:43:53 -0500

    created file structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// imports
+import { useState } from 'react'
+import './App.css'
+
+// app component function
+function App() {
+
+  // 
+  return (
+    <>
+      <section id="main">
+        
+      </section>
+    </>
+  )
+}
+
+export default App
